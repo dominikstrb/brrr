5a24ab48d241986319a0012d47595cd657906b7b
Dominik Straub <dstraub93@gmail.com>
2017-12-14 23:37:19 +0100

    Setup.py

--- brrr/skrrrahh.py
+++ brrr/skrrrahh.py
@@ -4,63 +4,62 @@ import random
 
 from .audio import play
 
-sounds = {"twochainz" : "twochainz.wav",
-            "twochainz1" : "twochainz1.wav",
-            "bigboi" : "bigboi.wav",
-            "biggie" : "biggie.wav",
-            "bigsean" : "bigsean.wav",
-            "bigsean1" : "bigsean1.wav",
-            "bigsean2" : "bigsean2.wav",
-            "bigsean3" : "bigsean3.wav",
-            "bigsean4" : "bigsean4.wav",
-            "bigsean5" : "bigsean5.wav",
-            "bigshaq" : "bigshaq.wav",
-            "birdman" : "birdman.wav",
-            "birdman1" : "birdman1.wav",
-            "birdman2" : "birdman2.wav",
-            "busta" : "busta.wav",
-            "chance" : "chance.wav",
-            "desiigner" : "desiigner.wav",
-            "diddy" : "diddy.wav",
-            "drake" : "drake.wav",
-            "drake1" : "drake1.wav",
-            "drummaboy" : "drummaboy.wav",
-            "fetty" : "fetty.wav",
-            "flava" : "flava.wav",
-            "future" : "future.wav",
-            "gucci" : "gucci.wav",
-            "gucci1" : "gucci1.wav",
-            "gucci2" : "gucci2.wav",
-            "jayz" : "jayz.wav",
-            "jayz1" : "jayz1.wav.wav",
-            "kendrick" : "kendrick.wav",
-            "khaled" : "khaled.wav",
-            "khaled1" : "khaled1.wav",
-            "khaled2" : "khaled2.wav",
-            "khaled3" : "khaled3.wav",
-            "liljon" : "liljon.wav",
-            "liljon1" : "liljon1.wav",
-            "nicki" : "nicki.wav",
-            "pitbull" : "pitbull.wav",
-            "ross" : "ross.wav",
-            "ross1" : "ross1.wav",
-            "schoolboy" : "schoolboy.wav",
-            "snoop" : "snoop.wav",
-            "soulja" : "soulja.wav",
-            "takeoff" : "takeoff.wav",
-            "tpain" : "tpain.wav",
-            "traviscott" : "traviscott.wav",
-            "treysongz" : "treysongz.wav",
-            "trick" : "trick.wav",
-            "waka" : "waka.wav",
-            "weezy" : "weezy.wav",
-            "yg" : "yg.wav"}
+sounds = {"twochainz": "twochainz.wav",
+          "twochainz1": "twochainz1.wav",
+          "bigboi": "bigboi.wav",
+          "biggie": "biggie.wav",
+          "bigsean": "bigsean.wav",
+          "bigsean1": "bigsean1.wav",
+          "bigsean2": "bigsean2.wav",
+          "bigsean3": "bigsean3.wav",
+          "bigsean4": "bigsean4.wav",
+          "bigsean5": "bigsean5.wav",
+          "bigshaq": "bigshaq.wav",
+          "birdman": "birdman.wav",
+          "birdman1": "birdman1.wav",
+          "birdman2": "birdman2.wav",
+          "busta": "busta.wav",
+          "chance": "chance.wav",
+          "desiigner": "desiigner.wav",
+          "diddy": "diddy.wav",
+          "drake": "drake.wav",
+          "drake1": "drake1.wav",
+          "drummaboy": "drummaboy.wav",
+          "fetty": "fetty.wav",
+          "flava": "flava.wav",
+          "future": "future.wav",
+          "gucci": "gucci.wav",
+          "gucci1": "gucci1.wav",
+          "gucci2": "gucci2.wav",
+          "jayz": "jayz.wav",
+          "jayz1": "jayz1.wav.wav",
+          "kendrick": "kendrick.wav",
+          "khaled": "khaled.wav",
+          "khaled1": "khaled1.wav",
+          "khaled2": "khaled2.wav",
+          "khaled3": "khaled3.wav",
+          "liljon": "liljon.wav",
+          "liljon1": "liljon1.wav",
+          "nicki": "nicki.wav",
+          "pitbull": "pitbull.wav",
+          "ross": "ross.wav",
+          "ross1": "ross1.wav",
+          "schoolboy": "schoolboy.wav",
+          "snoop": "snoop.wav",
+          "soulja": "soulja.wav",
+          "takeoff": "takeoff.wav",
+          "tpain": "tpain.wav",
+          "traviscott": "traviscott.wav",
+          "treysongz": "treysongz.wav",
+          "trick": "trick.wav",
+          "waka": "waka.wav",
+          "weezy": "weezy.wav",
+          "yg": "yg.wav"}
 
 PATH = 'adlibs/'
 
 
 def skrrrahh(sound=None):
-    sound_path = None
 
     if sound not in sounds.keys():
         if isinstance(sound, int) and sound < len(sounds):
@@ -69,11 +68,12 @@ def skrrrahh(sound=None):
             sound_path = sound
         else:
             if sound is not None:
-                warnings.warn('Sound must be either a valid sound indicator (see README), a number between 0 and {}, or a valid path to a sound file'.format(len(sounds) - 1))
+                warnings.warn(
+                    'Sound must be either a valid sound indicator (see README), a number between 0 and {}, or a valid path to a sound file'.format(
+                        len(sounds) - 1))
             sound = random.randint(0, len(sounds) - 1)
             sound_path = os.path.join(os.path.dirname(__file__), PATH, list(sounds.values())[sound])
     else:
         sound_path = os.path.join(os.path.dirname(__file__), PATH, sounds[sound])
-
 
     play(sound_path)
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'brrr',
+  packages = ['brrr'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Let Gucci Mane tell you when your script is done.',
+  author = 'Dominik Straub',
+  author_email = 'dstraub93@gmail.com',
+  url = 'https://github.com/dominikstrb/brrr', # use the URL to the github repo
+  download_url = 'https://github.com/dominikstrb/brrrr/archive/master.zip', # I'll explain this in a second
+  keywords = ['gucci', 'skrrrahh', 'brrr'], # arbitrary keywords
+  classifiers = [],
+  python_requires='>=3',
+)
