64f56d41ed1be1751350324d8a6718694b97b1df
Dominik Straub <dstraub93@gmail.com>
2017-12-15 12:13:47 +0100

    Add sound files to setup

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'brrr',
   packages = ['brrr'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.12',
   description = 'Let Gucci Mane tell you when your script is done.',
   author = 'Dominik Straub',
   author_email = 'dstraub93@gmail.com',
   url = 'https://github.com/dominikstrb/brrr', # use the URL to the github repo
-  download_url = 'https://github.com/dominikstrb/brrrr/archive/master.zip', # I'll explain this in a second
+  download_url = 'https://github.com/dominikstrb/brrr/archive/master.zip', # I'll explain this in a second
   keywords = ['gucci', 'skrrrahh', 'brrr'], # arbitrary keywords
   classifiers = [],
   python_requires='>=3',
+  package_data={'brrr': ['adlibs/*.wav']},
 )
